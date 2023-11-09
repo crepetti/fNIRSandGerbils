f83bb4ab86c3628640d8f41ce93369629b22af9a
emayaa <124737042+emayaa@users.noreply.github.com>
2023-11-09 11:47:04 -0500

    preprocessing updates with marking and interpolation

--- gerbilmaster_preprocessing.m
+++ gerbilmaster_preprocessing.m
@@ -4,14 +4,26 @@
 %taking raw BDF file and saving it at .set file
 %order = preprocessing, epoch, postprocessing, multsubjects
 %-------------------------------------------------------------------------------------------------------------------
-addpath('C:\Users\benri\Documents\eeglab2023.0');
-subID = 'newpilot912';
-filename = ['C:\Users\benri\Documents\GitHub\fNIRSandGerbils\', subID, '.bdf'];
+addpath('C:\Users\ema36\OneDrive\Documents\MATLAB\eeglab2023.0');
+pre_pro_epoched_data_folder = 'C:\Users\ema36\OneDrive\Documents\fNIRSandGerbils\prepro_epoched_data\';
+addpath(pre_pro_epoched_data_folder)
+subID = '7004';
+range_A = 'A4';
+range_B = 'B4';
+badchannels = 'channelsremoved.xlsx';
+BDF_filename = ['C:\Users\ema36\OneDrive\Documents\LiMN Things\Gerbil BDFs\', subID, '.bdf'];
+% pre_pro_epoched_data_folder = 'C:\Users\ema36\OneDrive\Documents\fNIRSandGerbils\prepro_epoched_data';
+% if ~exist(strcat('C:\Users\ema36\OneDrive\Documents\fNIRSandGerbils\prepro_epoched_data\'))%new folder to save preprocess data
+%         mkdir(strcat('C:\Users\ema36\OneDrive\Documents\fNIRSandGerbils\prepro_epoched_data\'))
+%         disp('exists');
+% else
+%     disp('does not exist');
+% end
 eeglab
 
 %loading in BDF files and re-referencing to externals (mastoids/earlobes)
 [ALLEEG EEG CURRENTSET ALLCOM] = eeglab;
-EEG = pop_biosig(filename, 'ref', [33 34], 'blockepoch', 'off', 'refoptions', {'keepref', 'off'});
+EEG = pop_biosig(BDF_filename, 'ref', [33 34], 'blockepoch', 'off', 'refoptions', {'keepref', 'off'});
 [ALLEEG EEG CURRENTSET] = pop_newset(ALLEEG, EEG, 0, 'gui', 'off');
 EEG = eeg_checkset( EEG );
 
@@ -22,7 +34,7 @@ EEG = eeg_checkset( EEG );
 
 %adding in channel locations - FOR NOW DOING LOAD, WILL EDIT LATER!
 % EEG=pop_chanedit(EEG, 'load',{'C:\Users\ema36\OneDrive\Documents\fNIRSandGerbils\richardson_32_chanlocs.locs' 'filetype' 'locs'});
-EEG=pop_chanedit(EEG, 'load',{'C:\Users\benri\Documents\GitHub\fNIRSandGerbils\chan_locs_cart.txt', 'filetype', 'sfp'});
+EEG=pop_chanedit(EEG, 'load',{'C:\Users\ema36\OneDrive\Documents\fNIRSandGerbils\chan_locs_cart.txt', 'filetype', 'sfp'});
 
 
 % [ALLEEG EEG CURRENTSET] = pop_newset(ALLEEG, EEG, 2, 'gui', 'off');
@@ -39,15 +51,23 @@ fs = EEG.srate;
 EEG.data = filtfilt(b, a, double(EEG.data')); 
 EEG.data = EEG.data';
 EEG = eeg_checkset( EEG );
+[ALLEEG EEG CURRENTSET] = pop_newset(ALLEEG, EEG, 3,'setname',[subID, 'Bandpassed'],'gui','on');
+
+%Marking out very obvious artifacts - pause here and manually do it
+disp('Clean Up Data Before Running ICA!');
+pause
+[ALLEEG EEG CURRENTSET] = pop_newset(ALLEEG, EEG, 3,'setname',[subID, 'artifactmarked'],'gui','on');
+EEG = pop_saveset( EEG, 'filename', [subID , '_artifactmarked.set'], 'filepath', pre_pro_epoched_data_folder);
+[ALLEEG EEG] = eeg_store(ALLEEG, EEG, CURRENTSET);
 
 %Running ICA
 % EEG = pop_runica(EEG, 'icatype', 'runcia', 'extended', 1, 'interrupt', 'on');
 % [ALLEEG EEG] = eeg_store(ALLEEG, EEG, CURRENTSET);
 % EEG = eeg_checkset( EEG );
 EEG = pop_runica(EEG, 'extended',1);
-[ALLEEG EEG CURRENTSET] = pop_newset(ALLEEG, EEG, 2,'setname',[subID, 'ICA'],'gui','on');
+[ALLEEG EEG CURRENTSET] = pop_newset(ALLEEG, EEG, 4,'setname',[subID, 'ICA'],'gui','on');
 
-EEG = pop_saveset( EEG, 'filename', [subID , '_ICAcomponentsin.set'], 'filepath', 'C:\Users\benri\Documents\GitHub\fNIRSandGerbils\prepro_epoched_data\');
+EEG = pop_saveset( EEG, 'filename', [subID , '_ICAcomponentsin.set'], 'filepath', pre_pro_epoched_data_folder);
 [ALLEEG EEG] = eeg_store(ALLEEG, EEG, CURRENTSET);
 
 pop_selectcomps(EEG,[1:32])
@@ -57,11 +77,27 @@ pause
 components_to_remove = input('Please enter a comma-separated list of ICA components to remove (ex. [1,2,3]):');
 EEG = pop_subcomp( EEG, components_to_remove, 0);
 close
-[ALLEEG EEG CURRENTSET] = pop_newset(ALLEEG, EEG, 3,'setname',[subID, 'ICA Cleaned'],'gui','on');
-
-%Saving ICA
-EEG = pop_saveset( EEG, 'filename', [subID , '_ICAdone.set'], 'filepath', 'C:\Users\benri\Documents\GitHub\fNIRSandGerbils\prepro_epoched_data\');
+[ALLEEG EEG CURRENTSET] = pop_newset(ALLEEG, EEG, 5,'setname',[subID, 'ICA Cleaned'],'gui','on');
+EEG = pop_saveset( EEG, 'filename', [subID , '_ICAcomponentsremoved.set'], 'filepath', pre_pro_epoched_data_folder);
 [ALLEEG EEG] = eeg_store(ALLEEG, EEG, CURRENTSET);
+
+%Rejecting/Interpolate Bad Channels - make sure to save # of bad channels
+%plot the graph
+pop_spectopo(EEG, 1);
+pause
+channels_to_remove = input('Please enter a comma-separated list of channels indices to interpolate (ex. [1 2 3]):');
+% EEG = pop_select(EEG, 'channel', channels_to_remove);
+EEG = pop_interp(EEG, channels_to_remove, 'spherical', [-1 16]);
+numchannels_removed = size(channels_to_remove, 2);
+writematrix(subID, badchannels, 'Sheet', 1,'Range', range_A);
+writematrix(numchannels_removed, badchannels, 'Sheet', 1, 'Range', range_B);
+[ALLEEG EEG CURRENTSET] = pop_newset(ALLEEG, EEG, 3,'setname',[subID, 'Channels Removed'],'gui','on');
+
+%Saving Overall Pre-Processed Data
+EEG = pop_saveset( EEG, 'filename', [subID , '_ICAdone.set'], 'filepath', pre_pro_epoched_data_folder);
+[ALLEEG EEG] = eeg_store(ALLEEG, EEG, CURRENTSET);
+
+disp('Done');
 
 eeglab redraw;
 
